59ec2ec8d8cbcba71bc704abf2ae01c41f8826ca
cc <cchen@artisreit.com>
2022-10-27 14:54:47 -0500

    save

--- index.js
+++ index.js
@@ -13,35 +13,10 @@ loadPosts().reduce((context,v,i)=>{
     return context
 },[])
 
-const cat = loadPosts().reduce((context,v)=>{
-    const index = context.ids.findIndex(id=>{
-        return v.categories.findIndex(i=>i===id) > 0
-    })
-    if(index > 0 ){
-        if(context.arr[v.id] === undefined){
-            context.arr[v.id] = []
-        }
-        context.arr[v.id].push(v)
-    }
-    return context;
-},{
-    ids:[...loadCategories().map(i=>i.id)],
-    arr:{}
+loadCategories().map(({id})=>{
+    const posts = loadPosts().filter(v=> v.categories.findIndex(i=>i===id) > 0)
+    fse.outputFileSync(`./out/data/category.${id}.json`,JSON.stringify(posts))
 })
-
-var key;
-for (key in cat.arr) {
-    if (cat.arr.hasOwnProperty(key)) {
-        fse.outputFileSync(`./out/data/category.${key}.json`,JSON.stringify(cat.arr[key]))
-    }
-}
-
 
 generator("pages","out","public")
 
-
-// for(let i=0;i<150;i++){
-//     if((i+1)%8 === 0){
-//         console.log(i,Math.floor(i/8))
-//     }
-// }
